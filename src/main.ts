c51bcfa68b08f35c8163af29204af9f4e33b2b8d
Jaesung Park <jaesung.cs@gmail.com>
2025-06-09 23:23:04 +0900

    spine

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import upsweepShaderCode from './shader/upsweep.wgsl?raw';
+import spineShaderCode from './shader/spine.wgsl?raw';
+
+const RADIX = 256;
 
 export default async function start() {
   const adapter = await navigator.gpu.requestAdapter();
-  const device = await adapter?.requestDevice();
+  const features = adapter!.features;
+  if (features.has("subgroups")) {
+    console.log("Subgroup operations supported!");
+  }
+
+  const device = await adapter?.requestDevice({
+    requiredFeatures: ["subgroups"],
+  });
   if (!device) throw new Error("WebGPU not supported");
 
   console.log("Adaptor:", adapter);
@@ -10,8 +20,11 @@ export default async function start() {
 
   const upsweepShaderModule = device.createShaderModule({ code: upsweepShaderCode });
   console.log("upsweep shader module:", upsweepShaderModule);
-  const info = await upsweepShaderModule.getCompilationInfo();
-  console.log(info);
+  console.log(await upsweepShaderModule.getCompilationInfo());
+
+  const spineShaderModule = device.createShaderModule({ code: spineShaderCode });
+  console.log("spine shader module:", spineShaderModule);
+  console.log(await spineShaderModule.getCompilationInfo());
 
   const elementCounts = device.createBuffer({ size: 4, usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST });
   const globalHistogram = device.createBuffer({ size: 4096 * 4, usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC });
@@ -19,7 +32,7 @@ export default async function start() {
   const keys = device.createBuffer({ size: 4 * 4, usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST });
   const sortPass = device.createBuffer({ size: 4, usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST });
 
-  let data = new Uint32Array([0, 1, 3, 2]);
+  let data = new Uint32Array([0, 1, 3, 1]);
   device.queue.writeBuffer(keys, 0, data.buffer, data.byteOffset, data.byteLength);
   data = new Uint32Array([4]);
   device.queue.writeBuffer(elementCounts, 0, data.buffer, data.byteOffset, data.byteLength);
@@ -61,10 +74,18 @@ export default async function start() {
     bindGroupLayouts: [storageBindGroupLayout, uniformBindGroupLayout]
   });
 
-  const pipeline = device.createComputePipeline({
+  const upsweepPipeline = device.createComputePipeline({
     layout: pipelineLayout,
     compute: {
       module: upsweepShaderModule,
+      entryPoint: "main"
+    }
+  });
+
+  const spinePipeline = device.createComputePipeline({
+    layout: pipelineLayout,
+    compute: {
+      module: spineShaderModule,
       entryPoint: "main"
     }
   });
@@ -95,12 +116,19 @@ export default async function start() {
   });
 
   const encoder = device.createCommandEncoder();
-  const pass = encoder.beginComputePass();
-  pass.setPipeline(pipeline);
-  pass.setBindGroup(0, storageBindGroup);
-  pass.setBindGroup(1, uniformBindGroup);
-  pass.dispatchWorkgroups(1);
-  pass.end();
+  const upsweepPass = encoder.beginComputePass();
+  upsweepPass.setPipeline(upsweepPipeline);
+  upsweepPass.setBindGroup(0, storageBindGroup);
+  upsweepPass.setBindGroup(1, uniformBindGroup);
+  upsweepPass.dispatchWorkgroups(1);
+  upsweepPass.end();
+
+  const spinePass = encoder.beginComputePass();
+  spinePass.setPipeline(spinePipeline);
+  spinePass.setBindGroup(0, storageBindGroup);
+  spinePass.setBindGroup(1, uniformBindGroup);
+  spinePass.dispatchWorkgroups(RADIX);
+  spinePass.end();
 
   device.queue.submit([encoder.finish()]);
 
---
+++ src/shader/spine.wgsl
@@ -0,0 +1,97 @@
+// See https://github.com/gpuweb/gpuweb/blob/main/proposals/subgroups.md for subgroups.
+enable subgroups;
+
+const RADIX = 256;
+const WORKGROUP_SIZE = 256;
+const PARTITION_DIVISION = 8;
+const PARTITION_SIZE = PARTITION_DIVISION * WORKGROUP_SIZE;
+const MAX_SUBGROUP_SIZE = 128;
+
+@binding(0) @group(0) var<storage, read> elementCounts: array<u32>;
+@binding(1) @group(0) var<storage, read_write> globalHistogram: array<u32>;
+@binding(2) @group(0) var<storage, read_write> partitionHistogram: array<u32>;
+
+@binding(0) @group(1) var<uniform> sortPass: u32;
+
+var<workgroup> reduction: u32;
+var<workgroup> intermediate: array<u32, MAX_SUBGROUP_SIZE>;
+
+// dispatch this shader (RADIX, 1, 1), so that gl_WorkGroupID.x is radix
+@compute
+@workgroup_size(WORKGROUP_SIZE, 1, 1)
+fn main(
+    @builtin(local_invocation_id) groupThreadID: vec3<u32>,
+    @builtin(workgroup_id) groupId: vec3<u32>,
+    @builtin(local_invocation_index) groupIndex: u32,
+    @builtin(subgroup_invocation_id) laneIndex: u32,  // 0..31
+    @builtin(subgroup_size) laneCount: u32,           // 32
+) {
+    let elementCount = elementCounts[0];
+
+    let waveIndex = groupIndex / laneCount;
+    let waveCount = WORKGROUP_SIZE / laneCount;
+    let index = waveIndex * laneCount + laneIndex;
+
+    let radix = groupId.x;
+
+    let partitionCount = (elementCount + PARTITION_SIZE - 1) / PARTITION_SIZE;
+
+    if index == 0 {
+        reduction = 0;
+    }
+    workgroupBarrier();
+
+    for (var i = 0u; WORKGROUP_SIZE * i < partitionCount; i++) {
+        let partitionIndex = WORKGROUP_SIZE * i + index;
+        let value = select(0, partitionHistogram[RADIX * partitionIndex + radix], partitionIndex < partitionCount);
+        var excl = subgroupExclusiveAdd(value) + reduction;
+        let sum = subgroupAdd(value);
+
+        if subgroupElect() {
+            intermediate[waveIndex] = sum;
+        }
+        workgroupBarrier();
+
+            {
+            let excl = subgroupExclusiveAdd(intermediate[index % MAX_SUBGROUP_SIZE]);
+            let sum = subgroupAdd(intermediate[index % MAX_SUBGROUP_SIZE]);
+            if index < waveCount {
+                intermediate[index] = excl;
+
+                if index == 0 {
+                    reduction += sum;
+                }
+            }
+        }
+        workgroupBarrier();
+
+        if partitionIndex < partitionCount {
+            excl += intermediate[waveIndex];
+            partitionHistogram[RADIX * partitionIndex + radix] = excl;
+        }
+        workgroupBarrier();
+    }
+
+    if radix == 0 {
+        // one workgroup is responsible for global histogram prefix sum
+        let value = globalHistogram[RADIX * sortPass + (index % RADIX)];
+        var excl = subgroupExclusiveAdd(value);
+        let sum = subgroupAdd(value);
+
+        if subgroupElect() {
+            intermediate[waveIndex] = sum;
+        }
+        workgroupBarrier();
+
+            {
+            let excl = subgroupExclusiveAdd(intermediate[index % MAX_SUBGROUP_SIZE]);
+            if index < RADIX / laneCount {
+                intermediate[index] = excl;
+            }
+        }
+        workgroupBarrier();
+
+        excl += intermediate[waveIndex];
+        globalHistogram[RADIX * sortPass + index] = excl;
+    }
+}
